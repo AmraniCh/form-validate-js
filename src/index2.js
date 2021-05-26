81ddec752202a4dcba7f05f3e86a8eab743181da
AmraniCh <elamrani.sv.laza@gmail.com>
2021-05-26 23:53:22 +0100

    Update index2.js

--- src/index2.js
+++ src/index2.js
@@ -1,145 +1,157 @@
-;(function() {
-
+(function () {
     var regex = {
             username: /^[a-z]+[0-9]*$/i,
             // RFC 2822
             email: /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/,
             numbers: /[0-9]+/,
-            tele: /[0-9]+/,
+            tele: /[0-9]+/
         },
-    
         // error messages
         messages = {
             en: {
                 match: 'Invalid format for {0} field value.',
                 required: 'The field {0} is required.',
                 max: 'The field {0} must not exceed {1} characters.',
-                equal: 'The field {0} not equals the value of field {1}',
+                equal: 'The field {0} not equals the value of field {1}'
             },
             fr: {
                 match: 'Le format du champ {0} est incorrect.',
                 required: 'Le champ {0} est requis.',
                 max: 'Le champ {0} ne doit pas dépasser {1} caractères.',
-                equal: 'The field {0} not equals the value of field {1}',
+                equal: 'The field {0} not equals the value of field {1}'
             }
         },
-        
         // supported events
-        supportedEvents = ['change', 'submit'],
-
+        defaultEvents = ['submit'],
         // showing errors after inputs
         showError = true,
-
+        // Highliting inputs
         inputHighlight = true;
-
         // default messages language
         lang = 'en',
-
         // supported constraints and their default values
-        constraints = {
+        (defaultConstraints = {
             required: false,
             match: null,
             maxlength: null,
             equal: null,
-        };
+            messages: messages
+        });
 
-    var FormValidator = function(form, options) {
+    var FormValidator = function (form, settings) {
         if (!(this instanceof FormValidator)) {
-            return new FormValidator(form, options);
+            return new FormValidator(form, settings);
         }
 
         if (!(form instanceof Element)) {
-            this.form = document.querySelector(form) || document.forms[0];
+            this.form = document.querySelector(form);
         }
 
-        // Build constraints options
-        var fields = this.form.querySelectorAll('input:not([type=submit]), select');
-
-        if (fields.length < 0) {
-            return false;
+        if (!settings || typeof settings !== 'object') {
+            return;
         }
 
-        this.constraints = {};
-
-        for (var key in fields) {
-            if (!fields.hasOwnProperty(key)) {
-                continue;
-            }
-            
-            var field = fields[key], optscts = options.constraints[field.name];
-
-            var cts = this.constraints[field.name] = {}
-                
-            // If field contraints are already given
-            if (field.name && options && options.constraints && optscts) {
-                Object.assign(this.constraints[field.name], constraints, optscts);
-            } else {
-                // check for HTML built-in validation attributes
-                var attributes = ['required', 'minlength', 'maxlength', 'min', 'max', 'pattern'], i = 0;
-
-                while(i < attributes.length) {
-                    var attr = attributes[i];
-
-                    // if the alternative constraint is not supported move to the next attribute
-                    if (Object.keys(constraints).indexOf(attr) === -1) {
-                        console.log(attr);
-                        i++;
-                    }
-                    
-                    switch(attr) {
-                        case 'required':
-                            cts[attr] = field.getAttribute('required') !== null;
-                            break;
-
-                        case 'maxlength':
-                            var val = field.getAttribute('maxlength');
-                            cts[attr] = val && Number.parseInt(val);
-                            break;
-
-                        case 'pattern':
-                            var val = field.getAttribute('pattern');
-                            cts[attr] = val && Number.parseInt(val);
-                            break;
-                    };
-
-                    i++;
-                }
-
-                // Check for type='email'
-            }
-        }
+        this.constraints = settings.constraints && this.initConstraints(settings.constraints);
+        this.events      = settings.events || defaultEvents;
+        this.lang        = settings.lang || this.lang;
     };
 
     FormValidator.prototype = {
 
-        getElementAttributes: function(element, attribute) {
-            var attributes = element.attributes,
-                results = {};
+        /** 
+         * Initializes validation constraints
+         * Handles unsupported validation constraints
+         * Handles the built-in HTML validation attributes
+         * @param  {Object}
+         * @return {Object}
+         */
+         initConstraints: function (constraints) {
+            var result = {};
 
-            for (var i = 0; i < attributes.length; i++) {
-                var attr = attributes[i].name;
-                if (attr === attribute) {
-                    results[attr] = true;
+            for (var key in constraints) {
+                if (!constraints.hasOwnProperty(key)) {
+                    continue;
                 }
-            } 
-            
-            return results;
-        }
 
-    };
+                // detected unsupported validation constraints and send a warn to the console
+                for(var _key in constraints[key]) {
+                    if (!constraints[key].hasOwnProperty(_key)
+                        || Object.keys(defaultConstraints).indexOf(_key) !== -1) {
+                        continue;
+                    }
+
+                    console.warn(_key + ' is unsupported validation constraint.');
+                    delete constraints[key][_key];
+                }
+
+                // ...
+                Object.assign(
+                    result[key] = {},
+                    defaultConstraints,
+                    constraints[key]
+                );
+
+                // check for HTML built-in validation attributes
+                var element = this.form[key];
+                (attributes = [
+                    'required',
+                    'minlength',
+                    'maxlength',
+                    'min',
+                    'max',
+                    'pattern'
+                ]),
+                (i = 0);
+
+                var cts = result[key];
+
+                while (i < attributes.length) {
+                    var attr = attributes[i];
+
+                    // if the attribute is not an HTML validation attribute move to the next one
+                    if (Object.keys(constraints).indexOf(attr) === -1) {
+                        break;
+                    }
+
+                    switch (attr) {
+                        case 'required':
+                            cts[attr] = element.getAttribute('required') !== null;
+                            break;
+
+                        case 'maxlength':
+                            var val = element.getAttribute('maxlength');
+                            cts[attr] = val && Number.parseInt(val);
+                            break;
+                    }
+
+                    // Check for type='email'
+
+                    i++;
+                }
+            }
+
+            return result;
+        },
+
     
+    };
+
     window.FormValidator = FormValidator;
 
-    console.log(FormValidator('#register_form', {
+    var validate = FormValidator('#register_form', {
         events: ['change', 'input'],
+        lang: 'fr',
         constraints: {
             username: {
-                required: true,
-                match: "tele",
-                max: 30
-            },
+                //required: true,
+                match: 'tele',
+                maxlength: 30,
+                mamak: 'mamak'
+            }
         }
-    }));
+    });
+
+    console.log(validate);
 
 })();
 
